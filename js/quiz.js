ecea31561eb99c6eee1f3d4915335b28e620e9ca
Atsuhiro Nakai <atsuh1r0.pro@gmail.com>
2022-08-02 22:20:14 +0900

    選択した際に強調するように修正

--- js/quiz.js
+++ js/quiz.js
@@ -12,21 +12,26 @@ const answers = [
 {
   for (let quizNum=1; quizNum <= 6; quizNum++) {
     let clickNum = 0;
-    document.getElementById('q' + quizNum + 'CorrectAnswer').addEventListener('click', () => {
+
+    const correctAnswer = document.getElementById('q' + quizNum + 'CorrectAnswer');
+    correctAnswer.addEventListener('click', () => {
       if (clickNum === 0) {
+        // クリックした選択肢を強調
+        correctAnswer.classList.add('emphasis-choice');
+        // 正解の表示
         const correctBox = document.createElement('div');
-        correctBox.classList.add('correctBox');
+        correctBox.classList.add('correct-box');
         correctBox.textContent = '正解！';
   
         const correctBoxText = document.createElement('div');
-        correctBoxText.classList.add('correctBoxText');
+        correctBoxText.classList.add('correct-box-text');
   
         const correctBoxA = document.createElement('div');
-        correctBoxA.classList.add('correctBoxA');
+        correctBoxA.classList.add('correct-box-A');
         correctBoxA.textContent = 'A';
   
         const correctBoxP = document.createElement('p');
-        correctBoxP.classList.add('correctBoxP');
+        correctBoxP.classList.add('correct-box-P');
         correctBoxP.textContent = answers[quizNum-1];
         
         const answerList = document.getElementById('q' + quizNum + 'AnswerList');
@@ -39,31 +44,38 @@ const answers = [
       }
     });
   
-    document.getElementById('q' + quizNum + 'IncorrectAnswer').addEventListener('click', () => {
-      if (clickNum === 0) {
-        const incorrectBox = document.createElement('div');
-        incorrectBox.classList.add('incorrectBox');
-        incorrectBox.textContent = '不正解...';
-  
-        const correctBoxText = document.createElement('div');
-        correctBoxText.classList.add('correctBoxText');
-  
-        const correctBoxA = document.createElement('div');
-        correctBoxA.classList.add('correctBoxA');
-        correctBoxA.textContent = 'A';
-  
-        const correctBoxP = document.createElement('p');
-        correctBoxP.classList.add('correctBoxP');
-        correctBoxP.textContent = answers[quizNum-1];
-        
-        const answerList = document.getElementById('q' + quizNum + 'AnswerList');
-        answerList.after(incorrectBox);
-        incorrectBox.appendChild(correctBoxText);
-        correctBoxText.appendChild(correctBoxA);
-        correctBoxText.appendChild(correctBoxP);
-  
-        clickNum++;
-      }
+    const incorrectAnswer = document.getElementsByClassName('q' + quizNum + 'IncorrectAnswer');
+    const incorrectAnswers = Array.from(incorrectAnswer);
+    incorrectAnswers.forEach(element => {
+      element.addEventListener('click', () => {
+        if (clickNum === 0) {
+          // クリックした選択肢の強調
+          element.classList.add('emphasis-choice');
+          // 不正解の表示
+          const incorrectBox = document.createElement('div');
+          incorrectBox.classList.add('incorrect-box');
+          incorrectBox.textContent = '不正解...';
+    
+          const correctBoxText = document.createElement('div');
+          correctBoxText.classList.add('correct-box-text');
+    
+          const correctBoxA = document.createElement('div');
+          correctBoxA.classList.add('correct-box-A');
+          correctBoxA.textContent = 'A';
+    
+          const correctBoxP = document.createElement('p');
+          correctBoxP.classList.add('correct-box-P');
+          correctBoxP.textContent = answers[quizNum-1];
+          
+          const answerList = document.getElementById('q' + quizNum + 'AnswerList');
+          answerList.after(incorrectBox);
+          incorrectBox.appendChild(correctBoxText);
+          correctBoxText.appendChild(correctBoxA);
+          correctBoxText.appendChild(correctBoxP);
+    
+          clickNum++;
+        }
+      });
     });
   }
 }
